e30a60a01117a2d3780350bd4694868bc3fb50ee
PawanGowda <35513880+pawankumarGowda@users.noreply.github.com>
2020-05-08 15:39:38 +0530

    updated

--- app/src/main/java/com/example/daggermvvm/network/view/RetroFragment.kt
+++ app/src/main/java/com/example/daggermvvm/network/view/RetroFragment.kt
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.PackageManager
 import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.os.Build
@@ -12,7 +11,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.PermissionChecker.checkSelfPermission
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -25,40 +23,44 @@ import com.example.daggermvvm.network.viewmodel.RetroViewModel
 import com.example.daggermvvm.network.viewmodel.RetroViewModelFactory
 import kotlinx.android.synthetic.main.post_list_layout.view.*
 import java.util.*
-import java.util.jar.Manifest
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.content.PermissionChecker
+import androidx.core.content.PermissionChecker.checkSelfPermission
 
-class RetroFragment: Fragment() {
+class RetroFragment : Fragment() {
     lateinit var retroViewModel: RetroViewModel
-    var fragmentView: View?=null
-    private  var listAdapter:PostListAdapter?=null
-    private var  postListLayoutBinding: PostListLayoutBinding?=null
+    var fragmentView: View? = null
+    private var listAdapter: PostListAdapter? = null
+    private var postListLayoutBinding: PostListLayoutBinding? = null
     private var wifiReceiverRegistered: Boolean = false
-    var wifiManager: WifiManager?= null
+    var wifiManager: WifiManager? = null
 
-    private val wifiReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            TODO("Not yet implemented")
-            val results = wifiManager?.scanResults as ArrayList<ScanResult>
-
-            if (results != null) {
-                listAdapter?.setAdapterList(WifiStation.newList(Collections.unmodifiableList(results)))
-            }
-        }
+    companion object {
+        private const val PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 120
     }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initViewModel()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        postListLayoutBinding = DataBindingUtil.inflate(inflater,
-            R.layout.post_list_layout,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        postListLayoutBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.post_list_layout, container, false
+        )
         fragmentView = postListLayoutBinding?.root
         initWifi()
         initAdapter()
         setAdapter()
         //fetchRetroInfo()
-        return  fragmentView
+        return fragmentView
     }
 
     override fun onStart() {
@@ -68,54 +70,74 @@ class RetroFragment: Fragment() {
 
     override fun onStop() {
         super.onStop()
-        if (wifiReceiverRegistered){
+        if (wifiReceiverRegistered) {
             activity?.unregisterReceiver(wifiReceiver)
             wifiReceiverRegistered = false
         }
-
-    }
-    private fun startScanning() {
-        activity?.registerReceiver(wifiReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
-        wifiReceiverRegistered = true
-        wifiManager?.startScan()
     }
 
-    fun initWifi(){
-       wifiManager = activity?.applicationContext?.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    private val wifiReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val results = wifiManager?.scanResults as ArrayList<ScanResult>
+
+            if (results != null) {
+                listAdapter?.setAdapterList(WifiStation.newList(Collections.unmodifiableList(results)))
+            }
+        }
+    }
+
+    fun initWifi() {
+        wifiManager =
+            context?.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
         if (wifiManager?.isWifiEnabled == false) {
-            wifiManager?.setWifiEnabled(true)
-        
-    }a
-    fun  initViewModel(){
-        var retroViewModelFactory = RetroViewModelFactory()
-        retroViewModel = ViewModelProviders.of(this,retroViewModelFactory).get(RetroViewModel::class.java)
+            wifiManager?.isWifiEnabled = true
+        }
     }
-    fun fetchRetroInfo(){
+
+    private fun startScanning() {
+        if (checkPermissions()) {
+            activity?.registerReceiver(
+                wifiReceiver,
+                IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+            )
+            wifiReceiverRegistered = true
+            wifiManager?.startScan()
+        }
+    }
+
+
+    fun initViewModel() {
+        var retroViewModelFactory = RetroViewModelFactory()
+        retroViewModel =
+            ViewModelProviders.of(this, retroViewModelFactory).get(RetroViewModel::class.java)
+    }
+
+    fun fetchRetroInfo() {
         /*retroViewModel.postInfoLiveData?.observe(this,object: Observer<List<PostInfo>> {
-            override fun onChanged(t: List<PostInfo>?) {
-                t?.apply {
-                    listAdapter?.setAdapterList(t)
-                }
+        override fun onChanged(t: List<PostInfo>?) {
+            t?.apply {
+                listAdapter?.setAdapterList(t)
             }
-        })*/
+        }
+    })*/
         /*val listPostInfo = listOf<PostInfo>(PostInfo("VCS2 APP NETWORK","Secure Network",""),
-            PostInfo("Connections Team network","Secure Network",""),
-            PostInfo("Trailer Team network","Secure Network",""),
-            PostInfo("Vehicle Team network","Secure Network",""),
-            PostInfo("Projection Team network","Secure Network",""),
-            PostInfo("Pawan","Secure Network",""),
-            PostInfo("Pratap network","Secure Network",""),
-            PostInfo("Sudhanshu Team network","Secure Network",""),
-            PostInfo("Chandrika Team network","Secure Network",""),
-            PostInfo("Chandrakanth Team network","Secure Network",""),
-            PostInfo("Harish","Secure Network","")
-        );*/
+        PostInfo("Connections Team network","Secure Network",""),
+        PostInfo("Trailer Team network","Secure Network",""),
+        PostInfo("Vehicle Team network","Secure Network",""),
+        PostInfo("Projection Team network","Secure Network",""),
+        PostInfo("Pawan","Secure Network",""),
+        PostInfo("Pratap network","Secure Network",""),
+        PostInfo("Sudhanshu Team network","Secure Network",""),
+        PostInfo("Chandrika Team network","Secure Network",""),
+        PostInfo("Chandrakanth Team network","Secure Network",""),
+        PostInfo("Harish","Secure Network","")
+    );*/
 
         //listAdapter?.setAdapterList(listPostInfo)
-
     }
-    private fun setAdapter(){
+
+    private fun setAdapter() {
         fragmentView?.post_list?.apply {
             layoutManager = LinearLayoutManager(activity)
             addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
@@ -123,7 +145,36 @@ class RetroFragment: Fragment() {
         }
     }
 
-    private fun initAdapter(){
+    private fun initAdapter() {
         listAdapter = PostListAdapter(this@RetroFragment.requireActivity())
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                this.requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) !== PermissionChecker.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION
+            )
+            return false
+        } else {
+            return true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION -> {
+                startScanning()
+            }
+        }
     }
 }
